6557d19780195717f349c3397f93c9f75cb121d9
Shin Umeda <umeda.shin@gmail.com>
2025-08-08 13:48:07 -0700

    Almost able to bootstrap itself. Just need to fix shim

--- crun.cpp
+++ crun.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iterator>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -46,7 +47,6 @@ constexpr auto build =
     Subcommand<"b", "build", "Builds a script">{}
         .arg(clip::help_arg)
         .arg(verbosity)
-        .arg(Argument<Str, flag, "o", "output", "Name of output file">{})
         .arg(Argument<Str, flag, none, "build-flags", "Flags to build with">{})
         .arg(compdb)
         .arg(compdb_path)
@@ -63,8 +63,8 @@ constexpr auto reg =
         .arg(Argument<std::vector<Str>, flag, none, "implicit-arg",
                       "Implicit arguments to pass to script">{})
         .arg(Argument<bool, flag, "D", "delete", "Delete executable shim">{})
-        .arg(
-            Argument<bool, flag, "l", "list", "List registered executables">{});
+        .arg(Argument<bool, flag, "l", "list", "List registered executables">{})
+        .arg(Argument<Str, positional, none, "script", "Script to register">{});
 
 constexpr auto comp =
     Subcommand<none, "completion", "Print the completion script for crun">{}
@@ -106,6 +106,7 @@ void completions_cmd(auto const &, std::size_t col);
 void bin_cmd(auto const &);
 
 fs::path cache_dir();
+fs::path config_dir();
 
 namespace lg {
 size_t level = 0;
@@ -176,8 +177,8 @@ void scan_deps(auto out, fs::path);
 void output_rule(auto out, fs::path src, std::string_view include);
 
 void build_cmd(auto const &args, std::size_t col) {
-  auto &[help, verbosity, output, flags, compdb, compdb_path, rebuild,
-         configure, script] = args;
+  auto &[help, verbosity, flags, compdb, compdb_path, rebuild, configure,
+         script] = args;
   if (help) {
     auto h = build.help({.cols = col});
     std::puts(h.c_str());
@@ -188,32 +189,39 @@ void build_cmd(auto const &args, std::size_t col) {
     throw Str("Specify a script to build");
   }
 
-  auto ninjafile = Str(ninja_prologue);
-  auto ninja = std::back_inserter(ninjafile);
   fs::path cache = cache_dir();
-  fs::path cwd = fs::current_path();
-  auto ninja_path = cache / cwd.relative_path() / script;
+  auto script_path = fs::absolute(script);
+  auto ninja_path = cache / script_path.relative_path();
   ninja_path.replace_extension(".ninja");
+  std::println("ninja path: {}", ninja_path.string());
+  fs::create_directories(ninja_path.parent_path());
+  auto ninjafile = std::ofstream(ninja_path);
+  auto ninja = std::ostreambuf_iterator(ninjafile);
 
+  std::ranges::copy(ninja_prologue, ninja);
   std::format_to(ninja, "build | {}: regenerate_ninja {}\n",
-                 ninja_path.string(), (cwd / script).string());
+                 ninja_path.string(), script_path.string());
 
-  auto deps = std::string_view(std::getenv("CRUN_DEPS"));
   auto root = std::getenv("CRUN_ROOT");
   scan_deps(ninja, fs::path(root) / "deps");
 
-  output_rule(ninja, cwd / script, "-I.");
+  output_rule(ninja, script_path, "-I.");
 
-  auto output_name = output.empty() ? fs::path(script).stem().string() : output;
+  auto output_name = fs::path(script).stem();
 
-  auto obj = cache / cwd.relative_path() / script;
+  auto obj = cache / script_path.relative_path();
   obj += ".o";
 
-  auto out_name = (cache / cwd.relative_path() / output_name).string();
-  std::format_to(ninja, "link {}: {}\n", out_name, obj.string());
+  auto out_name =
+      (cache / script_path.parent_path().relative_path() / output_name)
+          .string();
+  std::format_to(ninja, "build {}: link {}\n", out_name, obj.string());
   std::format_to(ninja, "default {}\n", out_name);
 
-  std::puts(ninjafile.c_str());
+  if (!configure) {
+    auto s = ninja_path.string();
+    sp::call({"ninja", "--quiet", "-f", s.c_str()});
+  }
 }
 
 void scan_deps(auto out, fs::path depdir) {
@@ -255,24 +263,55 @@ void output_rule(auto out, fs::path src, std::string_view include) {
       std::format_to(out, "gcm.cache/{}.gcm ", Str(p["logical-name"]));
     }
   }
-  std::format_to(out, " : {} ", filepath);
+  std::format_to(out, " : cxx {} ", filepath);
   if (req.size() > 0) {
     std::format_to(out, " | ");
     for (auto const &r : req) {
       std::format_to(out, "gcm.cache/{}.gcm ", Str(r["logical-name"]));
     }
   }
+  std::format_to(out, "\n  includes = {}", include);
+  std::format_to(out, "\n  deps = gcc");
+  auto basename = src.filename().string();
+  std::format_to(out, "\n  depflags = -MD -Mno-modules -MQ {0}.o -MF {0}.d",
+                 basename);
+  std::format_to(out, "\n  depfile = {}.d", basename);
   std::format_to(out, "\n\n");
 }
 
+// Todo windows??
 void register_cmd(auto const &args, std::size_t col) {
-  auto &[help, name, impl_args, del, list] = args;
+  auto &[help, name, impl_args, del, list, script_name] = args;
   if (help) {
     auto h = reg.help({.cols = col});
     std::puts(h.c_str());
     return;
   }
+  auto bin = config_dir() / "bin";
+  fs::create_directories(bin);
+  auto script = fs::absolute(script_name);
+  auto shim_name = name.empty() ? script.stem().string() : name;
+  auto shim_path = bin / shim_name;
+  auto shim = std::ofstream(shim_path);
+  auto reg = std::ostreambuf_iterator(shim);
+
+  constexpr std::string_view reg_script = "#!/usr/bin/sh\nset -euo pipefail\n";
+  std::ranges::copy(reg_script, reg);
+  auto binary = (cache_dir() / script.relative_path()).replace_extension("");
+  auto ninja = binary;
+  ninja.replace_extension(".ninja");
+  std::format_to(reg, "if [ ! -f {} ]; then crun build --configure {}; fi\n",
+                 ninja.string(), script.string());
+  std::format_to(reg, "ninja --quiet -f {}\n", ninja.string());
+  std::format_to(reg, "{} $@ ", binary.string());
+  for (auto const &arg : impl_args) {
+    std::format_to(reg, "{} ", arg);
+  }
+  *reg++ = '\n';
+  using enum fs::perms;
+  fs::permissions(shim_path, owner_exec, fs::perm_options::add);
 }
+
 void completions_cmd(auto const &args, std::size_t col) {
   auto &[help, shell] = args;
   if (help) {
@@ -281,7 +320,11 @@ void completions_cmd(auto const &args, std::size_t col) {
     return;
   }
 }
-void bin_cmd(auto const &args) {}
+
+void bin_cmd(auto const &args) {
+  auto bin = (config_dir() / "crun" / "bin").string();
+  std::puts(bin.c_str());
+}
 
 fs::path cache_dir() {
   // no need to redo this all the time,
@@ -296,10 +339,28 @@ fs::path cache_dir() {
     if (xdg_cache == nullptr || *xdg_cache == '\0') {
       if (home == nullptr || *home == '\0')
         throw std::runtime_error("HOME is null for some reason");
-      cached = fs::path(home) / ".cache";
+      cached = fs::path(home) / ".cache" / "crun";
     } else {
-      cached = fs::path(xdg_cache);
+      cached = fs::path(xdg_cache) / "crun";
     }
+  }
+  return *cached;
+}
+
+fs::path config_dir() {
+  // no need to redo this all the time,
+  // and this probably won't change mid-execution
+  static std::optional<fs::path> cached;
+  if (cached.has_value())
+    return *cached;
+  const char *xdg_config = std::getenv("XDG_CONFIG_HOME");
+  const char *home = std::getenv("HOME");
+  if (xdg_config == nullptr || *xdg_config == '\0') {
+    if (home == nullptr || *home == '\0')
+      throw std::runtime_error("HOME is null for some reason");
+    cached = fs::path(home) / ".config" / "crun";
+  } else {
+    cached = fs::path(xdg_config) / "crun";
   }
   return *cached;
 }
